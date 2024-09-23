379eb061048beb41b530d05d0f86431cf9c43f84
exam <exam@Aptechnk1.edu>
2024-09-23 18:13:52 +0500

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$con = mysqli_connect("localhost","root","","id22104765_zameenstate");
+	if (mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+?>
